0a1e044987e9d05eac1416d51748274fff0a3380
Simon Aytes <simon@aytes.net>
2022-04-26 15:21:16 -0400

    Added subway tracking

--- src/main.py
+++ src/main.py
@@ -9,6 +9,8 @@ import requests
 import datetime as dt
 import time
 import pandas as pd
+from google.transit import gtfs_realtime_pb2
+from protobuf_to_dict import protobuf_to_dict
 
 #################
 ### VARIABLES ###
@@ -20,13 +22,17 @@ bus_api_key, subway_api_key = ttu.getAPIKey()
 # Load in bus and subway ids
 bus_stop_id, subway_stop_id = ttu.getConfig()
 
+print(f"BUS : {bus_stop_id} \t SUBWAY : {subway_stop_id}")
+
 # NOTE: Bus stops are as follows
 # W 125 ST/MALCOM X BLVD
 # 402507 -- 125 ST TO COLUMBIA (WEST)
 # 402659 -- 125 ST TO LAGUARDIA (EAST)
 
 bus_stop_name = ttu.getBusStopName(bus_stop_id)
-subway_station_name = "TO DO"
+subway_station_name = ttu.getSubwayStationName(subway_stop_id)
+
+print(f"SUBWAY STATION NAME: {subway_station_name}")
 
 # Time between updates
 sleep_time = 10
@@ -42,26 +48,30 @@ stop_flag = False
 while not stop_flag:
     # Only run the main loop if there are no errors
     try:
-        # Gather bus data
-        bus_url = f"http://bustime.mta.info/api/siri/stop-monitoring.json?key={bus_api_key}&OperatorRef=MTA&MonitoringRef={bus_stop_id}"
-        bus_res = requests.request("GET", bus_url)
-        bus_json = json.loads(bus_res.text)
+        # Gather bus data & make bus stop object
+        bus_stop = ttu.BusStop(name=bus_stop_name, id=bus_stop_id, api_key=bus_api_key)
 
         # Gather subway data
-        # TODO
-
-        # Create bus stop object
-        bus_stop = ttu.BusStop(json=bus_json, name=bus_stop_name, id=bus_stop_id)
+        subway_headers = {"x-api-key": subway_api_key}
+        subway_url = ttu.getSubwayEndpoint("2")
+        subway_res = requests.request("GET", subway_url, headers=subway_headers)
+        feed = gtfs_realtime_pb2.FeedMessage()
+        feed.ParseFromString(subway_res.content)
+        subway_feed = protobuf_to_dict(feed) # subway_feed is a dictionary
+        subway_json = subway_feed['entity'] # train_data is a list
 
         # Create subway station object
-        # TODO
+        subway_station = ttu.SubwayStation(name=subway_station_name, id=subway_stop_id, data=subway_json)
 
         # Log time of last update
         last_update_time = dt.datetime.now()
 
         # DEBUG -- PRINT DATA
-        print(f"Data as of: {last_update_time}")
+        print(f"Data as of: {last_update_time}\n")
+        print("BUS SCHEDULE \n\n")
         bus_stop.getClosestBusses()
+        print("TRAIN SCHEDULE \n\n")
+        subway_station.getClosestTrains()
         print("-"*10)
 
         # Display results to screen
@@ -76,5 +86,5 @@ while not stop_flag:
         print(f"An error has occured and Transit Tracker has stopped running. See below for a detailed error message:\n\n{e}\n\n")
 
         # Set stop flag as true (will terminate loop)
-        stop_flag = True
+        #stop_flag = True
 
--- src/transit_tracker_utilities.py
+++ src/transit_tracker_utilities.py
@@ -5,10 +5,16 @@
 ####################
 
 import datetime as dt
+from re import sub
+from wsgiref.simple_server import sys_version
 from dateutil import parser
 from pytz import timezone
 import pandas as pd
+import requests
+import json
 import os
+from google.transit import gtfs_realtime_pb2
+from protobuf_to_dict import protobuf_to_dict
 
 #####################
 # UTILITY FUNCTIONS #
@@ -25,9 +31,12 @@ def getAPIKey():
     except: # If you don't have it set up, you will be prompted for it
         bus_api_key = input("INPUT BUS API KEY: ")
 
-    # Load the subway API key
-    # TODO
-    subway_api_key = "TO DO"
+    # Load the subway API key (Link to retrieve: https://api.mta.info/#/AccessKey)
+    try: # Try to load from `secrets` folder (will not be on everyone's system)
+        with open("./secrets/subway_api_key.txt") as f:
+            subway_api_key = next(f)
+    except: # If you don't have it set up, you will be prompted for it
+        subway_api_key = input("INPUT SUBWAY API KEY: ")
 
     # Return the API keys
     return bus_api_key, subway_api_key
@@ -50,6 +59,24 @@ def getBusStopName(bus_stop_id):
     # Return the name of the bus stop
     return bus_stops["stop_name"].tolist()[0]
 
+def getSubwayStationName(subway_stop_id):
+    # Load the subway station data
+    subway_stops = pd.read_csv("./config/subway_stops.csv")
+    subway_stops = subway_stops[subway_stops["stop_id"] == str(subway_stop_id)]
+    
+    # Return the name of the subway station
+    return subway_stops["stop_name"].tolist()[0]
+
+def getSubwayEndpoint(subway_line):
+    # Load the endpoint data
+    endpoints = pd.read_csv("./config/subway_api_endpoints.csv")
+    
+    # Find matching endpoint for subway line
+    endpoints = endpoints[endpoints['line'] == subway_line.lower()]
+    
+    # Return matching endpoint
+    return endpoints['endpoint'].tolist()[0]
+
 
 ###########
 # CLASSES #
@@ -62,17 +89,11 @@ class Bus():
     status = ""
     # Plaintext destination name
     destination = ""
-    # Time from stop (seconds) -- TODO: See code in BusStop() class
-    time_to_stop = 0.0
-    # Flag for whether the time is based off of expected or scheduled arrival time -- TODO: See code in BusStop() class
-    time_is_expected = 0
 
     def __init__(self, bus_id, presentable_distance, dest_name):
         self.line_id = bus_id # i.e. M60SBS, Bx15, etc.
         self.status = presentable_distance # i.e. 5 stops away, approaching, at stop, etc.
         self.destination = dest_name # i.e. W. HARLEM BWAY - 125 via 3 AV
-        # self.time_to_stop = bus_time_dist # time in seconds
-        # self.time_is_expected = is_expected
 
 class BusStop():
     # List of closest busses
@@ -84,7 +105,7 @@ class BusStop():
     # Direction of travel (for reference only)
     direction = ""
 
-    def __init__(self, json, name, id):
+    def __init__(self, name, id, api_key):
         # Assign name
         self.stop_name = name
 
@@ -97,8 +118,11 @@ class BusStop():
         # Clear busses list
         self.busses = []
 
+        # Make API call
+        json_obj = self.__getBusData__(api_key)
+
         # Get closes three busses to stop
-        closest_busses = json["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]['MonitoredStopVisit'][0:3]
+        closest_busses = json_obj["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]['MonitoredStopVisit'][0:3]
 
         for bus in closest_busses:
             # Expected vs. Aimed flag
@@ -113,24 +137,16 @@ class BusStop():
             # Destination Name
             t_destination_name = bus['MonitoredVehicleJourney']['DestinationName']
 
-            # Time to stop -- TODO: API reference was not clear and this is making no sense atm. Will try again later.
-            #if "ExpectedArrivalTime" in bus["MonitoredVehicleJourney"]["MonitoredCall"]:
-            #    print(bus["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"])
-            #    t_expected_arrival = parser.parse(bus["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"])
-            #    t_expected_arrival = t_expected_arrival.replace(tzinfo=timezone('EST'))
-            #    t_is_expected = 1
-            #else:
-            #    t_expected_arrival = parser.parse(bus["MonitoredVehicleJourney"]["MonitoredCall"]["AimedArrivalTime"])
-            #    t_expected_arrival = t_expected_arrival.replace(tzinfo=timezone('EST'))
-            #    t_is_expected = 1
-    
-            #curr_time = dt.datetime.now()
-            #curr_time = curr_time.replace(tzinfo=timezone('EST'))
-            #self.t_time_to_stop = (t_expected_arrival - curr_time).seconds
-
             # Create Bus object and store in `busses` list
-            #self.busses.append(Bus(t_line_id, t_status, t_expected_arrival, t_is_expected))
             self.busses.append(Bus(t_line_id, t_status, t_destination_name))
+
+    def __getBusData__(self, api_key):
+        # Gather bus data
+        bus_url = f"http://bustime.mta.info/api/siri/stop-monitoring.json?key={api_key}&OperatorRef=MTA&MonitoringRef={self.stop_id}"
+        bus_res = requests.request("GET", bus_url)
+
+        # Return the JSON data
+        return json.loads(bus_res.text)
 
     def determineDirection(self):
         # Load config file
@@ -138,7 +154,7 @@ class BusStop():
 
         # Grab matching bus
         config = config[config['id'] == self.stop_id]
-        
+
         # Return direction from config file
         return config['direction'].tolist()[0]
 
@@ -146,3 +162,137 @@ class BusStop():
         print(f"STOP: {self.stop_name} (id: {self.stop_id})\tDIR: {self.direction}\n")
         for bus in self.busses:
             print(f"ID: {bus.line_id}\tSTATUS: {bus.status}\tDEST: {bus.destination}\n")
+
+class SubwayTrain():
+    # Plaintext subway line name
+    line_id = ""
+    # Distance status (e.g. 5 stops away, approaching, etc.)
+    status = ""
+    # Plaintext destination name
+    destination = ""
+    # Train type (Uptown or Downtown)
+    direction = ""
+
+    def __init__(self, presentable_distance, route_id, subway_direction):
+        self.line_id = route_id # i.e. 1, 7, N, etc.
+        self.status = presentable_distance # i.e. 5 stops away, approaching, at stop, etc.
+        self.destination = self.__getRouteText__(route_id) # i.e. 7 AVE EXPRESS
+        self.direction = subway_direction # UPT or DWT
+
+    def __getRouteText__(self, route_id):
+        # Load route data
+        subway_routes = pd.read_csv("./config/subway_routes.csv")
+        
+        # Find route name
+        subway_routes = subway_routes[subway_routes['route_id'] == route_id]
+
+        # Return route name
+        return subway_routes["route_long_name"].tolist()[0]
+
+class SubwayStation():
+    # List of closest trains
+    trains = []
+    # ID of the station
+    stop_id = ""
+    # Plaintext station name
+    stop_name = ""
+
+    def __init__(self, name, id, data):
+        # Assign name
+        self.stop_name = name
+
+        # Assign Id
+        self.stop_id = str(id)
+
+        # Get a dataframe with the first 5 arriving trains
+        subway_data = self.__parseSubwayData__(data).head(5)
+
+        # Clear trains list
+        self.trains = []
+
+        # Loop throuh subway data and create train objects
+        for index, row in subway_data.iterrows():
+            # Parse inputs from dataframe
+            t_distance = self.__getPresentableDistance__(row['arrival_time'], row['departure_time'])
+            t_route_id = row['route']
+            t_direction = self.__getTrainDirection__(row['stop'])
+
+            # Create train object and append to list
+            self.trains.append(SubwayTrain(t_distance, t_route_id, t_direction))
+
+    def __getTrainDirection__(self, stop):
+        if stop[-1] == "N":
+            return "UPT"
+        else:
+            return "DWT"
+
+    def __getPresentableDistance__(self, arrivalTime, departureTime):
+        curr_time = dt.datetime.now()
+
+        if arrivalTime <= curr_time:
+            if departureTime > curr_time:
+                return("At station")
+        elif arrivalTime >= curr_time:
+            t_diff = arrivalTime - curr_time
+            if t_diff.total_seconds() % 60 <= 0:
+                return "<1 min"
+            else:
+                return f"{str(int(t_diff.total_seconds() / 60))} min"
+        else:
+            return("At station")
+
+    def __parseSubwayData__(self, data):
+        # Create empty lists
+        stop_ids = []
+        route_ids = []
+        arrival_times = []
+        departure_times = []
+
+        # Loop through train data and grab info
+        for train in data:
+            if 'trip_update' in train:
+                t_route_id = train['trip_update']['trip']['route_id']
+                for stop in train['trip_update']['stop_time_update']:
+                    if self.stop_id in stop['stop_id']:
+                        # Grab stop id
+                        stop_ids.append(stop['stop_id'])
+                        
+                        # Grab route id
+                        route_ids.append(t_route_id)
+                        
+                        # Grab arrival time
+                        
+                        arrival_times.append(dt.datetime.fromtimestamp(stop['arrival']['time']))
+                        
+                        # Grab departure time
+                        departure_times.append(dt.datetime.fromtimestamp(stop['departure']['time']))
+            else:
+                pass
+        
+        # Create dataframe
+        train_arrival_data = pd.DataFrame()
+        train_arrival_data['stop'] = stop_ids
+        train_arrival_data['route'] = route_ids
+        train_arrival_data['arrival_time'] = arrival_times
+        train_arrival_data['departure_time'] = departure_times
+
+        # Sort data by arrival time
+        train_arrival_data = train_arrival_data.sort_values(by=['arrival_time'])
+
+        # Return sorted data by arrival time
+        return train_arrival_data
+
+    def determineDirection(self):
+        # Load config file
+        config = pd.read_csv("./config/config.csv")
+
+        # Grab matching bus
+        config = config[config['id'] == self.stop_id]
+
+        # Return direction from config file
+        return config['direction'].tolist()[0]
+
+    def getClosestTrains(self):
+        print(f"STOP: {self.stop_name} (id: {self.stop_id})\n")
+        for train in self.trains:
+            print(f"ID: {train.line_id}\tSTATUS: {train.status}\tDEST: {train.destination} \tDIR: {train.direction}\n")
