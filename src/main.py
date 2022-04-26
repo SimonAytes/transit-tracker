778afca56d0d9bd22ea62dd3b4e2feee002bea55
Simon Aytes <simon@aytes.net>
2022-04-25 23:52:40 -0400

    Added base functionality for bus tracking

--- src/main.py
+++ src/main.py
@@ -1,1 +1,56 @@
+# main.py
+
+###################
+# IMPORT LIBARIES #
+###################
 import transit_tracker_utilities as ttu
+import json
+import requests
+import datetime as dt
+import time
+
+#################
+### VARIABLES ###
+#################
+
+# API Key
+bus_api_key = "YOUR API KEY HERE"
+
+# Load in API key
+with open("./secrets/bus_api_key.txt") as f:
+    bus_api_key = next(f)
+
+# Choose bus stop
+bus_stop_id = "402659" # W 125 ST/MALCOM X BLVD
+        # 402507 -- 125 ST TO COLUMBIA (WEST)
+        # 402659 -- 125 ST TO LAGUARDIA (EAST)
+bus_stop_name = "W 125 ST/MALCOM X BLVD"
+
+# Time between updates
+sleep_time = 10
+
+# Flag to stop the loop (will only occur if there is an error)
+stop_flag = False
+
+#########################
+### MAIN PROGRAM LOOP ###
+#########################
+
+while not stop_flag:
+    # Loop structure
+    last_update_time = dt.datetime.now()
+
+    # Gather bus data
+    bus_url = f"http://bustime.mta.info/api/siri/stop-monitoring.json?key={bus_api_key}&OperatorRef=MTA&MonitoringRef={bus_stop_id}"
+    bus_res = requests.request("GET", bus_url)
+    bus_json = json.loads(bus_res.text)
+    bus_stop = ttu.BusStop(json=bus_json, name=bus_stop_name, id=bus_stop_id)
+
+    # DEBUG -- PRINT DATA
+    print(f"Data as of: {last_update_time}")
+    bus_stop.GetClosestBusses()
+    print("_"*10)
+
+    # Sleep for a set amount of time
+    time.sleep(sleep_time)
+
--- src/transit_tracker_utilities.py
+++ src/transit_tracker_utilities.py
@@ -1,2 +1,81 @@
-def empty_fnc():
-    pass
+# Import libraries
+import datetime as dt
+from dateutil import parser
+from pytz import timezone
+
+class Bus():
+    # Plaintext bus line name
+    line_id = ""
+    # Distance status (e.g. 5 stops away, approaching, etc.)
+    status = ""
+    # Plaintext destination name
+    destination = ""
+    # Time from stop (seconds) -- TO DO: See code in BusStop() class
+    time_to_stop = 0.0
+    # Flag for whether the time is based off of expected or scheduled arrival time -- TO DO: See code in BusStop() class
+    time_is_expected = 0
+
+    def __init__(self, bus_id, presentable_distance, dest_name):
+        self.line_id = bus_id # i.e. M60SBS, Bx15, etc.
+        self.status = presentable_distance # i.e. 5 stops away, approaching, at stop, etc.
+        self.destination = dest_name # i.e. W. HARLEM BWAY - 125 via 3 AV
+        # self.time_to_stop = bus_time_dist # time in seconds
+        # self.time_is_expected = is_expected
+
+class BusStop():
+    # List of closest busses
+    busses = []
+    # ID of the stop
+    stop_id = ""
+    # Plaintext stop name
+    stop_name = ""
+
+    def __init__(self, json, name, id):
+        # Assign name
+        self.stop_name = name
+
+        # Assign Id
+        self.stop_id = id
+
+        # Clear busses list
+        self.busses = []
+
+        # Get closes three busses to stop
+        closest_busses = json["Siri"]["ServiceDelivery"]["StopMonitoringDelivery"][0]['MonitoredStopVisit'][0:3]
+
+        for bus in closest_busses:
+            # Expected vs. Aimed flag
+            #t_is_expected = 0
+
+            # Bus Line ID
+            t_line_id = bus["MonitoredVehicleJourney"]["LineRef"].split("_")[1]
+
+            # Bus Status
+            t_status = bus["MonitoredVehicleJourney"]["MonitoredCall"]["Extensions"]["Distances"]["PresentableDistance"]
+
+            # Destination Name
+            t_destination_name = bus['MonitoredVehicleJourney']['DestinationName']
+
+            # Time to stop -- TO DO: API reference was not clear and this is making no sense atm. Will try again later.
+            #if "ExpectedArrivalTime" in bus["MonitoredVehicleJourney"]["MonitoredCall"]:
+            #    print(bus["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"])
+            #    t_expected_arrival = parser.parse(bus["MonitoredVehicleJourney"]["MonitoredCall"]["ExpectedArrivalTime"])
+            #    t_expected_arrival = t_expected_arrival.replace(tzinfo=timezone('EST'))
+            #    t_is_expected = 1
+            #else:
+            #    t_expected_arrival = parser.parse(bus["MonitoredVehicleJourney"]["MonitoredCall"]["AimedArrivalTime"])
+            #    t_expected_arrival = t_expected_arrival.replace(tzinfo=timezone('EST'))
+            #    t_is_expected = 1
+    
+            #curr_time = dt.datetime.now()
+            #curr_time = curr_time.replace(tzinfo=timezone('EST'))
+            #self.t_time_to_stop = (t_expected_arrival - curr_time).seconds
+
+            # Create Bus object and store in `busses` list
+            #self.busses.append(Bus(t_line_id, t_status, t_expected_arrival, t_is_expected))
+            self.busses.append(Bus(t_line_id, t_status, t_destination_name))
+
+    def GetClosestBusses(self):
+        print(f"STOP: {self.stop_name} (id: {self.stop_id})\n")
+        for bus in self.busses:
+            print(f"ID: {bus.line_id}\tSTATUS: {bus.status}\tDEST: {bus.destination}\n")
